99f70520a9610459e3e74590a12fd523a0a07952
Alexandre Serex <alexandre.serex@he-arc.ch>
2016-09-26 16:58:15 +0200

    Added the perspective trick to the triangle. Not sure it works with every model though, and it's still not perfect. And the camera code choice is awful.

--- squelette/jsExos/ALEX_CH04.js
+++ squelette/jsExos/ALEX_CH04.js
@@ -5,11 +5,17 @@
 var vertexBuffer = null;
 var indexBuffer = null;
 var colorBuffer = null;
+
 var indices = [];
 var vertices = [];
 var colors = [];
+
 var mvMatrix = mat4.create();
 var pMatrix = mat4.create();
+
+var withPerspective = true;
+var rotationAroundZ = 0;
+var indexCounter = 0;
 
 window.onload = displayTitle("Ch04_ex1");
 
@@ -88,7 +94,7 @@ function triangleCeption() {
     colors.push(0.0, 0.0, 1.0, 1.0);
     colors.push(0.0, 0.0, 1.0, 1.0);
 
-    for(var i = 1; i<= 100; i++) {
+    for(var i = 1; i<= 30; i++) {
         var Apoint = [vertices[9*i-9], vertices[9*i-8], vertices[9*i-7]];
         var Bpoint = [vertices[9*i-6], vertices[9*i-5], vertices[9*i-4]];
         var Cpoint = [vertices[9*i-3], vertices[9*i-2], vertices[9*i-1]];
@@ -105,10 +111,14 @@ function triangleCeption() {
 
         indices.push(i-1);
     }
+
+    window.setInterval(function() {
+        rotationAroundZ += 0.1;
+        indexCounter++;
+    }, 16);
 }
 
-function midPoint(A, B, portion)
-{
+function midPoint(A, B, portion) {
     var ABVector = [(B[0] - A[0]), (B[1] - A[1]), (B[2] - A[2])];
     return [(A[0] + ABVector[0]/portion), (A[1] + ABVector[1]/portion), (A[2] + ABVector[2]/portion)];
 }
@@ -151,6 +161,7 @@ function createSquare(botLeftX, botLeftY, size) {
 }
 
 function drawScene() {
+    var a, b, c;
     glContext.clearColor(0.9, 0.9, 0.9, 1.0);
     glContext.enable(glContext.DEPTH_TEST);
     glContext.clear(glContext.COLOR_BUFFER_BIT | glContext.DEPTH_BUFFER_BIT);
@@ -158,6 +169,26 @@ function drawScene() {
 
     mat4.identity(pMatrix);
     mat4.identity(mvMatrix);
+
+    if (withPerspective) {
+        mat4.perspective(pMatrix, degToRad(60), c_width / c_height, 0.1, 10000);
+        b = 0.1 * Math.cos(rotationAroundZ);
+        a = 0.3 + 0.1 * Math.sin(rotationAroundZ);
+        c = -2;
+        translationMat = mat4.create();
+        mat4.identity(translationMat);
+        mat4.translate(translationMat, translationMat, [b, a, c]);
+        mat4.multiply(mvMatrix, translationMat, mvMatrix);
+        mvMatrix = mat4.rotateY(mvMatrix, mvMatrix, Math.PI);
+    } else {
+        b = 0.1 * Math.cos(rotationAroundZ);
+        a = 0.3 + 0.1 * Math.sin(rotationAroundZ);
+        c = 0;
+        translationMat = mat4.create();
+        mat4.identity(translationMat);
+        mat4.translate(translationMat, translationMat, [b, a, c]);
+        mat4.multiply(mvMatrix, translationMat, mvMatrix);
+    }
 
     glContext.uniformMatrix4fv(prg.pMatrixUniform, false, pMatrix);
     glContext.uniformMatrix4fv(prg.mvMatrixUniform, false, mvMatrix);
@@ -186,3 +217,11 @@ function initWebGL() {
     finally {
     }
 }
+
+function changeProjectionMode() {
+    if (withPerspective) {
+        withPerspective = 0
+    } else {
+        withPerspective = 1
+    }
+};
