14b73f822c8feb1f2023c3bc5d1cd9d6cdf22c66
Karim Luy <karim.luy@he-arc.ch>
2016-10-04 15:20:34 +0200

    commented all lines used for creating a color in the pipeline JS => FragmentShader as they are not used anywhere

--- squelette/jsExos/KARIM_CH04_ChessBoard_Hot.js
+++ squelette/jsExos/KARIM_CH04_ChessBoard_Hot.js
@@ -4,12 +4,11 @@
 
 var vertexBuffer = null;
 var indexBuffer = null;
-var colorBuffer = null;
-var pointsBuffer = null;
+//var colorBuffer = null; //color buffer not needed
 
 var indices = [];
 var vertices = [];
-var colors = [];
+//var colors = []; //colors not needed
 
 var mvMatrix = mat4.create();
 var pMatrix = mat4.create();
@@ -38,8 +37,9 @@ function initShaderParameters(prg) {
     prg.vertexPositionAttribute = glContext.getAttribLocation(prg, "aVertexPosition");
     glContext.enableVertexAttribArray(prg.vertexPositionAttribute);
 
-    prg.colorAttribute = glContext.getAttribLocation(prg, "aColor");
-    glContext.enableVertexAttribArray(prg.colorAttribute);
+	//no need for color bridge anymore
+    //prg.colorAttribute = glContext.getAttribLocation(prg, "aColor");
+    //glContext.enableVertexAttribArray(prg.colorAttribute);
 
     prg.pMatrixUniform = glContext.getUniformLocation(prg, "uPMatrix");
     prg.mvMatrixUniform = glContext.getUniformLocation(prg, "uMVMatrix");
@@ -50,7 +50,7 @@ function initBuffers() {
     createSquare();
 	
     vertexBuffer = getVertexBufferWithVertices(vertices);
-    colorBuffer = getVertexBufferWithVertices(colors);
+    //colorBuffer = getVertexBufferWithVertices(colors);
     indexBuffer = getIndexBufferWithIndices(indices);
 }
 
@@ -63,11 +63,11 @@ function createSquare() {
 	vertices.push(-1.0,-1.0,0.0);
 	vertices.push(1.0,-1.0,0.0);
 	
-	//generate white for each corner of the Chessboard
-    colors.push(1.0, 1.0, 1.0, transparency);
-    colors.push(1.0, 1.0, 1.0, transparency);
-    colors.push(1.0, 1.0, 1.0, transparency);
-    colors.push(1.0, 1.0, 1.0, transparency);
+	//colors not necessary anymore as they are handled by fragshader
+    //colors.push(1.0, 1.0, 1.0, transparency);
+    //colors.push(1.0, 1.0, 1.0, transparency);
+    //colors.push(1.0, 1.0, 1.0, transparency);
+    //colors.push(1.0, 1.0, 1.0, transparency);
 	//pronounce triangles draw order
 	indices.push(0,1,2,2,3,0);
 }
@@ -80,8 +80,8 @@ function drawScene() {
     glContext.bindBuffer(glContext.ARRAY_BUFFER, vertexBuffer);
     glContext.vertexAttribPointer(prg.vertexPositionAttribute, 3, glContext.FLOAT, false, 0, 0);
 
-    glContext.bindBuffer(glContext.ARRAY_BUFFER, colorBuffer);
-    glContext.vertexAttribPointer(prg.colorAttribute, 4, glContext.FLOAT, false, 0, 0);
+    //glContext.bindBuffer(glContext.ARRAY_BUFFER, colorBuffer);
+    //glContext.vertexAttribPointer(prg.colorAttribute, 4, glContext.FLOAT, false, 0, 0);
 
     glContext.bindBuffer(glContext.ELEMENT_ARRAY_BUFFER, indexBuffer);
     glContext.drawElements(glContext.TRIANGLES, indices.length, glContext.UNSIGNED_SHORT, 0);
