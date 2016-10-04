bf7ea5c19336bf220843bb9b938fa14eb8416ca9
Karim Luy <karim.luy@he-arc.ch>
2016-10-04 09:04:14 +0200

    started chessboard pattern, yes it looks terrible but i need to code something simple and repetitive for debugging purposes

--- squelette/jsExos/KARIM_CH04_ChessBoard_Hot.js
+++ squelette/jsExos/KARIM_CH04_ChessBoard_Hot.js
@@ -5,6 +5,7 @@
 var vertexBuffer = null;
 var indexBuffer = null;
 var colorBuffer = null;
+var pointsBuffer = null;
 
 var indices = [];
 var vertices = [];
@@ -17,13 +18,20 @@ var translation = vec3.create();
 window.onload = displayTitle("Ch04_ex6");
 
 
-function initCamera() {
-    mat4.identity(mvMatrix);
+function initCamera(){
+	mat4.identity(mvMatrix);
+	
+	mat4.perspective (pMatrix, 45.0, c_width / c_height, 0.1, 100.0);
+			
+	vec3.set (translation, 0.0, 0.0, -2.0);
+	mat4.translate (mvMatrix, mvMatrix, translation);
+}
 
-    mat4.perspective(45, c_width / c_height, 0.0, 100.0, pMatrix);
-
-    vec3.set(translation, 0.0, 0.0, -0.1);
-    mat4.translate(mvMatrix, mvMatrix, translation);
+function initScene(){
+	glContext.clearColor(0.1, 0.1, 0.1, 1.0);
+    glContext.enable(glContext.DEPTH_TEST);
+    glContext.clear(glContext.COLOR_BUFFER_BIT | glContext.DEPTH_BUFFER_BIT);
+    glContext.viewport(0, 0, c_width, c_height);
 }
 
 function initShaderParameters(prg) {
@@ -33,17 +41,14 @@ function initShaderParameters(prg) {
     prg.colorAttribute = glContext.getAttribLocation(prg, "aColor");
     glContext.enableVertexAttribArray(prg.colorAttribute);
 
-    prg.pMatrixUniform = glContext.getUniformLocation(prg, 'uPMatrix');
-    prg.mvMatrixUniform = glContext.getUniformLocation(prg, 'uMVMatrix');
+    prg.pMatrixUniform = glContext.getUniformLocation(prg, "uPMatrix");
+    prg.mvMatrixUniform = glContext.getUniformLocation(prg, "uMVMatrix");
+	
 }
 
 function initBuffers() {
     createSquare();
-    //DEBUG HELP
-    if (vertices.length != (colors.length - (vertices.length / 3))) {
-        throw new BadInitBufferException("Vertices and colors need to be the same length");
-    }
-
+	
     vertexBuffer = getVertexBufferWithVertices(vertices);
     colorBuffer = getVertexBufferWithVertices(colors);
     indexBuffer = getIndexBufferWithIndices(indices);
@@ -52,30 +57,26 @@ function initBuffers() {
 
 function createSquare() {
     var transparency = 1.0;
-    //create 4 vertices for Chessboard corners
-    vertices.push(1.0, 1.0, 0);
-    vertices.push(1.0, -1.0, 0);
-    vertices.push(-1.0, -1.0, 0);
-    vertices.push(-1.0, 1.0, 0);
-    //generate white for each corner of the Chessboard
+	//create 4 vertices for Chessboard corners
+	vertices.push(1.0,1.0,0.0);
+	vertices.push(-1.0,1.0,0.0);
+	vertices.push(-1.0,-1.0,0.0);
+	vertices.push(1.0,-1.0,0.0);
+	
+	//generate white for each corner of the Chessboard
     colors.push(1.0, 1.0, 1.0, transparency);
     colors.push(1.0, 1.0, 1.0, transparency);
     colors.push(1.0, 1.0, 1.0, transparency);
     colors.push(1.0, 1.0, 1.0, transparency);
-    //pronounce triangle strip draw order
-    indices.push(0, 1, 2, 3);
+	//pronounce triangle strip draw order
+	indices.push(0,1,2,2,3,0);
 }
 
 function drawScene() {
-
-    glContext.clearColor(0.0, 0.0, 0.0, 1.0);
-    glContext.enable(glContext.DEPTH_TEST);
-    glContext.clear(glContext.COLOR_BUFFER_BIT | glContext.DEPTH_BUFFER_BIT);
-    glContext.viewport(0, 0, c_width, c_height);
-
+	initScene();
     glContext.uniformMatrix4fv(prg.pMatrixUniform, false, pMatrix);
     glContext.uniformMatrix4fv(prg.mvMatrixUniform, false, mvMatrix);
-
+	
     glContext.bindBuffer(glContext.ARRAY_BUFFER, vertexBuffer);
     glContext.vertexAttribPointer(prg.vertexPositionAttribute, 3, glContext.FLOAT, false, 0, 0);
 
@@ -83,8 +84,7 @@ function drawScene() {
     glContext.vertexAttribPointer(prg.colorAttribute, 4, glContext.FLOAT, false, 0, 0);
 
     glContext.bindBuffer(glContext.ELEMENT_ARRAY_BUFFER, indexBuffer);
-    glContext.drawElements(glContext.TRIANGLE_STRIP, indices.length, glContext.UNSIGNED_SHORT, 0);
-
+    glContext.drawElements(glContext.TRIANGLES, indices.length, glContext.UNSIGNED_SHORT, 0);
 }
 
 function initWebGL() {
