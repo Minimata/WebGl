e877fdf3ae5c2b3b6aa5dbdc08ffc5faa00fe78b
Alexandre Serex <alexandre.serex@he-arc.ch>
2016-10-04 07:51:49 +0200

    Corrected render method error.

--- squelette/jsExos/KARIM_CH04_ChessBoard_Hot.js
+++ squelette/jsExos/KARIM_CH04_ChessBoard_Hot.js
@@ -17,13 +17,13 @@ var translation = vec3.create();
 window.onload = displayTitle("Ch04_ex6");
 
 
-function initCamera(){
-	mat4.identity(mvMatrix);
-	
-	mat4.perspective (45, c_width / c_height, 0.0, 100.0, pMatrix);
-	
-	vec3.set (translation, 0.0, 0.0, -0.1);
-	mat4.translate (mvMatrix, mvMatrix, translation);
+function initCamera() {
+    mat4.identity(mvMatrix);
+
+    mat4.perspective(45, c_width / c_height, 0.0, 100.0, pMatrix);
+
+    vec3.set(translation, 0.0, 0.0, -0.1);
+    mat4.translate(mvMatrix, mvMatrix, translation);
 }
 
 function initShaderParameters(prg) {
@@ -52,22 +52,22 @@ function initBuffers() {
 
 function createSquare() {
     var transparency = 1.0;
-	//create 4 vertices for Chessboard corners
-	vertices.push(1.0,1.0,0);
-	vertices.push(1.0,-1.0,0);
-	vertices.push(-1.0,-1.0,0);
-	vertices.push(-1.0,1.0,0);
-	//generate white for each corner of the Chessboard
+    //create 4 vertices for Chessboard corners
+    vertices.push(1.0, 1.0, 0);
+    vertices.push(1.0, -1.0, 0);
+    vertices.push(-1.0, -1.0, 0);
+    vertices.push(-1.0, 1.0, 0);
+    //generate white for each corner of the Chessboard
     colors.push(1.0, 1.0, 1.0, transparency);
     colors.push(1.0, 1.0, 1.0, transparency);
     colors.push(1.0, 1.0, 1.0, transparency);
     colors.push(1.0, 1.0, 1.0, transparency);
-	//pronounce triangle strip draw order
-	indices.push(0,1,2,3);
+    //pronounce triangle strip draw order
+    indices.push(0, 1, 2, 3);
 }
 
 function drawScene() {
-    
+
     glContext.clearColor(0.0, 0.0, 0.0, 1.0);
     glContext.enable(glContext.DEPTH_TEST);
     glContext.clear(glContext.COLOR_BUFFER_BIT | glContext.DEPTH_BUFFER_BIT);
@@ -83,16 +83,20 @@ function drawScene() {
     glContext.vertexAttribPointer(prg.colorAttribute, 4, glContext.FLOAT, false, 0, 0);
 
     glContext.bindBuffer(glContext.ELEMENT_ARRAY_BUFFER, indexBuffer);
-    glContext.drawElements(glContext.GL_TRIANGLE_STRIP, indices.length, glContext.UNSIGNED_SHORT, 0);
+    glContext.drawElements(glContext.TRIANGLE_STRIP, indices.length, glContext.UNSIGNED_SHORT, 0);
+
 }
 
 function initWebGL() {
-    try{
-	glContext = getGLContext('webgl-canvas');
-	initProgram();
-	initCamera();
-	initBuffers();
-	renderLoop();}
-	catch(e){console.log(e)}
-    
+    try {
+        glContext = getGLContext('webgl-canvas');
+        initProgram();
+        initCamera();
+        initBuffers();
+        renderLoop();
+    }
+    catch (e) {
+        console.error(e)
+    }
+
 }
